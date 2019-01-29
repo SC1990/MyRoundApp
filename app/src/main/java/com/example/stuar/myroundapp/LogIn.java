12d74635ee0951903dfa24384bb5d3617aa32e46
SC1990 <stuartcarroll91@gmail.com>
2019-01-29 14:37:52 +0000

    ...

--- app/src/main/java/com/example/stuar/myroundapp/LogIn.java
+++ app/src/main/java/com/example/stuar/myroundapp/LogIn.java
@@ -2,10 +2,12 @@ package com.example.stuar.myroundapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LogIn extends AppCompatActivity {
 
@@ -24,6 +31,10 @@ public class LogIn extends AppCompatActivity {
     private ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+    private String userType;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +50,41 @@ public class LogIn extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser() != null){
-            finish();
-            startActivity(new Intent(getApplicationContext(), UserHome.class));
+            databaseReference = FirebaseDatabase.getInstance().getReference("users/customers").child(firebaseAuth.getUid());
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    User user = dataSnapshot.getValue(User.class);
+                    try{
+                        userType = user.getUserType();
+
+                        if (user.getUserType().equals("cust")){
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), UserHome.class));
+                            Toast.makeText(LogIn.this, "Logged in", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }catch(Exception e){
+                        NullPointerException nullPointerException;
+
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), RetailerHome.class));
+                        Toast.makeText(LogIn.this, "Logged in", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
         }
+
+        user = new User();
+
     }
 
     private void userLogin(){
@@ -55,29 +98,64 @@ public class LogIn extends AppCompatActivity {
             Toast.makeText(this, "Please enter all details", Toast.LENGTH_LONG).show();
             progressDialog.dismiss();
         }
-        else{
-            firebaseAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            progressDialog.dismiss();
+        else firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
 
-                            if(task.isSuccessful()){
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), UserHome.class));
-                                Toast.makeText(LogIn.this, "logged in", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-                                Toast.makeText(LogIn.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                            progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+
+                            databaseReference = FirebaseDatabase.getInstance().getReference("users/customers").child(firebaseAuth.getUid());
+                            // Attach a listener to read the data at your profile reference
+                            databaseReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                    User user = dataSnapshot.getValue(User.class);
+                                    try{
+                                        userType = user.getUserType();
+
+                                        if (user.getUserType().equals("cust")){
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), UserHome.class));
+                                            Toast.makeText(LogIn.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                        }
+
+                                    }catch(Exception e){
+                                        NullPointerException nullPointerException;
+
+                                        finish();
+                                        startActivity(new Intent(getApplicationContext(), RetailerHome.class));
+                                        Toast.makeText(LogIn.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
+
+                        } else {
+                            Toast.makeText(LogIn.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    });
+                        progressDialog.dismiss();
+                    }
+                });
+    }
 
-
-
+    public void linkUserToAppropriatePage(String userType){
+        if(userType.equals("cust")){
+            startActivity(new Intent(getApplicationContext(), UserHome.class));
+        }
+        else if(userType.equals("ret")){
+            startActivity(new Intent(getApplicationContext(), RetailerHome.class));
         }
     }
+
 
     public void onLogInBtnClick(View view){
         if(view.getId() == R.id.logInBtn){
