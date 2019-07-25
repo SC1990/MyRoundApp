8f65f3eeddf620b052cb328cdb23fa8a7728f0e2
SC1990 <stuartcarroll91@gmail.com>
2019-07-25 20:56:22 +0100

    carttt

--- app/src/main/java/com/example/stuar/myroundapp/ConfirmOrderActivity.java
+++ app/src/main/java/com/example/stuar/myroundapp/ConfirmOrderActivity.java
@@ -1,43 +1,35 @@
 package com.example.stuar.myroundapp;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.RadioGroup;
 
-import com.example.stuar.myroundapp.CustomerActivities.CustomerHome;
-import com.example.stuar.myroundapp.CustomerActivities.RetailerProfileCustView;
 import com.example.stuar.myroundapp.DataRetrieval.RememberMe;
-import com.example.stuar.myroundapp.DataRetrieval.RetailerDetails;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
 
     private EditText orderName, orderPhone, orderAddress, orderTown;
-    private Button confirmBtn;
+    private Button goToPayBtn;
 
     private String total = "";
 
     private String saveCurrentDate;
     private String saveCurrentTime;
 
+    private RadioGroup radioGroup;
+
+    private String paymentOption = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
 
-        confirmBtn = findViewById(R.id.confirm_btn);
+        goToPayBtn = findViewById(R.id.confirm_btn);
         orderName = findViewById(R.id.order_name);
         orderPhone= findViewById(R.id.order_phonenum);
         orderAddress = findViewById(R.id.order_address);
@@ -50,66 +42,49 @@ public class ConfirmOrderActivity extends AppCompatActivity {
 
         total = getIntent().getStringExtra("total");
 
-        confirmBtn.setOnClickListener(new View.OnClickListener() {
+        goToPayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                confirmOrder();
+                goToPayment();
                 RememberMe.cartCount = 0;
+            }
+        });
+
+        radioGroup = findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.radio0:
+                        paymentOption = "card";
+                        break;
+                    case R.id.radio1:
+                        paymentOption = "cash";
+                        break;
+
+                }
             }
         });
     }
 
-    private void confirmOrder() {
 
-        Calendar calendar = Calendar.getInstance();
+    private void goToPayment() {
 
-        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MM-yyyy");
-        saveCurrentDate = currentDate.format(calendar.getTime());
+        if(paymentOption.equals("card")){
+            Intent intent = new Intent(ConfirmOrderActivity.this, CustomerPaymentActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            Intent intent = new Intent(ConfirmOrderActivity.this, CustomerOrderActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
 
-        SimpleDateFormat currentTime = new SimpleDateFormat("HH-mm-ss");
-        saveCurrentTime = currentTime.format(calendar.getTime());
 
-        final DatabaseReference orderRef = FirebaseDatabase.getInstance().getReference()
-                .child("Orders")
-                .child(RememberMe.currentOnlineUser.getPhone());
-
-        HashMap<String, Object> ordersMap = new HashMap<>();
-        ordersMap.put("total", total);
-        ordersMap.put("name", RememberMe.currentOnlineUser.getName());
-        ordersMap.put("phone", RememberMe.currentOnlineUser.getPhone());
-        ordersMap.put("address", RememberMe.currentOnlineUser.getAddress());
-        ordersMap.put("rId", RetailerDetails.retailerId);
-        ordersMap.put("date", saveCurrentDate);
-        ordersMap.put("time", saveCurrentTime);
-        ordersMap.put("status", "CustomerOrder Placed");
-
-        orderRef.updateChildren(ordersMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-
-                if(task.isSuccessful()){
-                    FirebaseDatabase.getInstance().getReference()
-                            .child("Cart")
-                            .child("Customer view")
-                            .child(RememberMe.currentOnlineUser.getPhone())
-                            .removeValue()
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(getApplicationContext(), "CustomerOrder Placed", Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(ConfirmOrderActivity.this, CustomerHome.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-
-                                }
-                            });
-                }
-
-            }
-        });
     }
 }
--- app/src/main/java/com/example/stuar/myroundapp/CustomerPaymentActivity.java
+++ app/src/main/java/com/example/stuar/myroundapp/CustomerPaymentActivity.java
@@ -1,6 +1,7 @@
 
 package com.example.stuar.myroundapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,9 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.stuar.myroundapp.CustomerActivities.CustomerHome;
 import com.example.stuar.myroundapp.DataRetrieval.RememberMe;
+import com.example.stuar.myroundapp.DataRetrieval.RetailerDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.stripe.android.Stripe;
@@ -19,7 +24,10 @@ import com.stripe.android.model.Card;
 import com.stripe.android.model.Token;
 import com.stripe.android.view.CardMultilineWidget;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Currency;
+import java.util.HashMap;
 
 import static java.security.AccessController.getContext;
 
@@ -37,6 +45,11 @@ public class CustomerPaymentActivity extends AppCompatActivity {
 
     private Token token;
 
+    private String saveCurrentDate;
+    private String saveCurrentTime;
+
+    private String total = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +64,8 @@ public class CustomerPaymentActivity extends AppCompatActivity {
         payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                createOrder();
 
                 if(mCardMultilineWidget.getCard()!= null){
 
@@ -122,6 +137,57 @@ public class CustomerPaymentActivity extends AppCompatActivity {
 
     }
 
+    private void createOrder() {
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MM-yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH-mm-ss");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        final DatabaseReference orderRef = FirebaseDatabase.getInstance().getReference()
+                .child("Orders")
+                .child(RememberMe.currentOnlineUser.getPhone());
+
+        HashMap<String, Object> ordersMap = new HashMap<>();
+        ordersMap.put("total", total);
+        ordersMap.put("name", RememberMe.currentOnlineUser.getName());
+        ordersMap.put("phone", RememberMe.currentOnlineUser.getPhone());
+        ordersMap.put("address", RememberMe.currentOnlineUser.getAddress());
+        ordersMap.put("rId", RetailerDetails.retailerId);
+        ordersMap.put("date", saveCurrentDate);
+        ordersMap.put("time", saveCurrentTime);
+        ordersMap.put("status", "CustomerOrder Placed");
+
+        orderRef.updateChildren(ordersMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+                if(task.isSuccessful()){
+                    FirebaseDatabase.getInstance().getReference()
+                            .child("Cart")
+                            .child("Customer view")
+                            .child(RememberMe.currentOnlineUser.getPhone())
+                            .removeValue()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(getApplicationContext(), "CustomerOrder Placed", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(CustomerPaymentActivity.this, CustomerHome.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+
+                                }
+                            });
+                }
+
+            }
+        });
+    }
 
 
     private void handleError(String s) {
